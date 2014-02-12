ea35bd32cfb3f4878306e3dac468f9f3c8074a66
firefoxmmx2 <firefoxmmx@gmail.com>
2014-02-12 23:39:49 +0800

    添加系统和菜单模型

---
+++ test/SystemManageSpec.scala
@@ -0,0 +1,12 @@
+import org.specs2.mutable._
+
+/**
+ * Created by hooxin on 14-2-12.
+ */
+class SystemManageSpec extends Specification{
+   "系统管理模块" should {
+     "添加一个机构" in {
+
+     }
+   }
+}
