c2911ee8a066043f738290fe3ad02627bea69a8c
firefoxmmx2 <firefoxmmx@gmail.com>
2014-03-05 17:23:29 +0800

    添加背景图片， 修改测试用例

--- test/SystemManageSpec.scala
+++ test/SystemManageSpec.scala
@@ -1,3 +1,4 @@
+import dao.Departments
 import models.Department
 import org.specs2.mutable._
 import scala.util.Try
@@ -5,11 +6,13 @@ import scala.util.Try
 /**
  * Created by hooxin on 14-2-12.
  */
-class SystemManageSpec extends Specification{
-   "系统管理模块" should {
-     "添加一个机构" in {
-       val department=Department(9999,"csjd","测试节点",0,"csjd.",1)
-
-     }
-   }
+class SystemManageSpec extends Specification {
+  "系统管理模块" should {
+    "添加一个机构" in {
+      val department = Department(9999, "csjd", "测试节点", 0, "csjd.", 1,
+        departallpin = None, departsimplepin = None, departbrevitycode = "csjd")
+      val insertedDepartment = Departments.insert(department)
+      assert(department == insertedDepartment)
+    }
+  }
 }
