3b8294f68f2e1a74ba51951e5171db41ad27aeff
firefoxmmx2 <firefoxmmx@gmail.com>
2014-03-16 22:03:12 +0800

    添加用户dao

--- app/dao/systemmanage/SystemManageDaoTrait.scala
+++ app/dao/systemmanage/SystemManageDaoTrait.scala
@@ -1,17 +1,26 @@
 package dao.systemmanage
 
 import dao.BaseDao
-import models.Department
+import models.{User, Department}
 
 /**
  * Created by hooxin on 14-3-10.
  */
 
-trait DepartmentDaoComponent{
-  val departmentDao:DepartmentDao
+trait DepartmentDaoComponent {
+  val departmentDao: DepartmentDao
 
-  trait DepartmentDao extends BaseDao[Department,Long]{
+  trait DepartmentDao extends BaseDao[Department, Long] {
 
   }
+
+}
+
+
+trait UserDaoComponent {
+  val userDao: UserDao
+
+  trait UserDao extends BaseDao[User, Long]
+
 }
 
