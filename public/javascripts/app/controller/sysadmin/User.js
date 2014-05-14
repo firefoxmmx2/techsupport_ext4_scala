9fbc6b8d3ef28405109680180fac8639918bfff7
firefoxmmx2 <firefoxmmx@gmail.com>
2014-05-14 17:25:03 +0800

    用户列表内容自动设置高度

--- public/javascripts/app/controller/sysadmin/User.js
+++ public/javascripts/app/controller/sysadmin/User.js
@@ -4,10 +4,24 @@
 Ext.define('Techsupport.controller.sysadmin.User', {
     extend: 'Ext.app.Controller',
     views: ['sysadmin.user.Add', 'sysadmin.user.List'],
-    stores: ['User','YN'],
+    stores: ['User', 'YN'],
+//    refs:[
+//        {ref:'userlist',selector:''}
+//    ],
     init: function () {
         this.control({
+            'gridpanel': {
+                afterrender: function (g, eOpts) {
+                    var dockedHeight = g.getDockedItems().map(function (x) {
+                        return x.getHeight();
+                    }).reduce(function (x, y) {
+                        return x + y;
+                    });
 
+                    g.setHeight(g.ownerCt.ownerCt.body.getHeight()
+                        - dockedHeight - g.getEl().getMargin('tb'));
+                }
+            }
         });
     }
 })
