68a5f5f471cdfd1681674b53c23b1281c522fed1
firefoxmmx2 <firefoxmmx@gmail.com>
2014-11-27 00:48:06 +0800

    机构树组件选择全选效果只作用于多选

--- public/javascripts/app/controller/sysadmin/MenuTree.js
+++ public/javascripts/app/controller/sysadmin/MenuTree.js
@@ -45,14 +45,14 @@ Ext.define('Techsupport.controller.sysadmin.MenuTree', {
                     }
                     store.getProxy().setExtraParam("parentmenucode", tree.cdata.menucode);
                 },
-                itemclick: function (v, record, item, index, e, eOpts) {
-                    var tree = v.ownerCt;
-                    tree.cdata.menucode = record.raw.id;
-                    tree.cdata.menuname = record.raw.text;
-                    tree.cdata.menulevel = record.raw.menulevel;
-                    tree.cdata.menufullcode = record.raw.menufullcode;
-                    tree.cdata.systemcode = record.raw.systemcode;
-                },
+                //itemclick: function (v, record, item, index, e, eOpts) {
+                //    var tree = v.ownerCt;
+                //    tree.cdata.menucode = record.raw.id;
+                //    tree.cdata.menuname = record.raw.text;
+                //    tree.cdata.menulevel = record.raw.menulevel;
+                //    tree.cdata.menufullcode = record.raw.menufullcode;
+                //    tree.cdata.systemcode = record.raw.systemcode;
+                //},
                 itemcontextmenu: function (view, record, item, index, e, eOpts) {
                     //右键菜单
                     e.preventDefault();
@@ -76,38 +76,44 @@ Ext.define('Techsupport.controller.sysadmin.MenuTree', {
                     tree.cdata.menulevel = record.raw.menulevel;
                     tree.cdata.menufullcode = record.raw.menufullcode;
                     tree.cdata.systemcode = record.raw.systemcode;
-                    var node = tree.getStore().getNodeById(record.data.id)
-                    if(!record.data.leaf){
-                        if(node.data.expandable){
-                            node.expand(false, function () {
-                                tree.getSelectionModel().select(node.childNodes,true)
-                            })
+
+                    //当选择模式为多选的时候启用
+                    if(tree.getSelectionModel().getSelectionMode() == 'MULTI'){
+                        var node = tree.getStore().getNodeById(record.data.id)
+                        if(!record.data.leaf){
+                            if(node.data.expandable){
+                                node.expand(false, function () {
+                                    tree.getSelectionModel().select(node.childNodes,true)
+                                })
+                            }
                         }
-                    }
-                    else {
-                        tree.getSelectionModel().select(node.parentNode,true,true)
+                        else {
+                            tree.getSelectionModel().select(node.parentNode,true,true)
+                        }
                     }
                 },
                 deselect: function (model, record, index, eOpts) {
                     var tree=model.treeStore.ownerTree
                     var node=tree.getStore().getNodeById(record.data.id)
-                    if(!record.data.leaf){
-                        if(node.hasChildNodes()){
-                            tree.getSelectionModel().deselect(node.childNodes)
+                    if(tree.getSelectionModel().getSelectionMode() == 'MULTI'){
+                        if(!record.data.leaf){
+                            if(node.hasChildNodes()){
+                                tree.getSelectionModel().deselect(node.childNodes)
+                            }
                         }
-                    }
-                    else{
-                        //当子菜单不再被选中的时候,也清理掉父菜单的选中状态.
-                        if(node.parentNode ){
-                            var selectedResults=Ext.Array.map(node.parentNode.childNodes, function (r) {
-                                return !tree.getSelectionModel().isSelected(r)
-                            })
-                            var result=true;
-                            Ext.Array.each(selectedResults, function (r) {
-                                result=result && r
-                            })
-                            if(result)
-                                tree.getSelectionModel().deselect(node.parentNode)
+                        else{
+                            //当子菜单不再被选中的时候,也清理掉父菜单的选中状态.
+                            if(node.parentNode ){
+                                var selectedResults=Ext.Array.map(node.parentNode.childNodes, function (r) {
+                                    return !tree.getSelectionModel().isSelected(r)
+                                })
+                                var result=true;
+                                Ext.Array.each(selectedResults, function (r) {
+                                    result=result && r
+                                })
+                                if(result)
+                                    tree.getSelectionModel().deselect(node.parentNode)
+                            }
                         }
                     }
                 }
