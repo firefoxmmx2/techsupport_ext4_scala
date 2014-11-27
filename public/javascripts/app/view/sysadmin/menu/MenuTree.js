db90a66254bda251c5cc8ae4049b55399681d6fa
firefoxmmx2 <firefoxmmx@gmail.com>
2014-11-28 00:05:02 +0800

    菜单树也使用比较时尚的箭头展开图标

--- public/javascripts/app/view/sysadmin/menu/MenuTree.js
+++ public/javascripts/app/view/sysadmin/menu/MenuTree.js
@@ -5,5 +5,6 @@ Ext.define('Techsupport.view.sysadmin.menu.MenuTree', {
     extend: 'Ext.tree.Panel',
     store: 'MenuTree',
     border: false,
+    useArrows: true,
     alias: 'widget.menutree'
 });
