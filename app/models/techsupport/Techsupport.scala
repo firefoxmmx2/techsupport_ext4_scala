c244c18eb3469c78f0adeb27131cad69d5453707
firefoxmmx2 <firefoxmmx@gmail.com>
2014-12-03 17:32:38 +0800

    添加对应的equals 和 hashcode

--- app/models/techsupport/Techsupport.scala
+++ app/models/techsupport/Techsupport.scala
@@ -97,70 +97,150 @@ case class Archive(
  * @param feedbackApproval
  * @param archive
  */
-case class SupportTicket(
-                          baseTicket: BaseSupportTicket,
-                          departmentApproval: Option[DepartmentApproval],
-                          trackingApproval: Option[TrackingApproval],
-                          feedbackApproval: Option[Feedback],
-                          archive: Option[Archive],
-                          lTrackings: Option[List[Tracking]],
-                          lSupportDepartments: Option[Department],
-                          lSupportLeaders: Option[User]
-                          )
+case class SupportTicketAll(
+                             baseTicket: BaseSupportTicket,
+                             departmentApproval: Option[DepartmentApproval],
+                             trackingApproval: Option[TrackingApproval],
+                             feedbackApproval: Option[Feedback],
+                             archive: Option[Archive],
+                             lTrackings: Option[List[Tracking]],
+                             lSupportDepartments: Option[Department],
+                             lSupportLeaders: Option[User]
+                             )
 
-class SupportTicketTable(
-                          @Column("applicant")
-                          val applicantId: Long,
-                          @Column("support_content")
-                          val supportContent: String,
-                          @Column("st_status")
-                          val stStatus: String,
-                          val region: String,
-                          @Column("serial_number")
-                          val serialNumber: Int,
-                          @Column("dev_sche_date")
-                          val devScheDate: Option[DateTime],
-                          @Column("psg_sche_date")
-                          val psgScheDate: Option[DateTime],
-                          @Column("dev_ds_sche_date")
-                          val devDsScheDate: Option[DateTime],
-                          @Column("dev_dd_sche_date")
-                          val devDdScheDate: Option[DateTime],
-                          @Column("dev_dt_sche_date")
-                          val devDtScheDate: Option[DateTime],
-                          @Column("psg_ds_sche_date")
-                          val psgDsScheDate: Option[DateTime],
-                          @Column("psg_Is_sche_date")
-                          val psgIsScheDate: Option[DateTime],
-                          @Column("applying_feedback_date")
-                          val applyingFeedbackDate: Option[DateTime],
-                          @Column("psg_comp_date")
-                          val psgCompDate: Option[DateTime],
-                          @Column("dev_comp_date")
-                          val devCompDate: Option[DateTime],
-                          @Column("psg_ds_comp_date")
-                          val psgDsCompDate: Option[DateTime],
-                          @Column("psg_is_comp_date")
-                          val psgIsCompDate: Option[DateTime],
-                          @Column("dev_ds_comp_date")
-                          val devDsCompDate: Option[DateTime],
-                          @Column("dev_dd_comp_date")
-                          val devDdCompDate: Option[DateTime],
-                          @Column("dev_dt_comp_date")
-                          val devDtCompDate: Option[DateTime],
-                          @Column("feedback_confirm_date")
-                          val feedbackConfirmDate: Option[DateTime],
-                          val comments: Option[String],
-                          @Column("archive_code")
-                          val archiveCode: Option[String],
-                          @Column("archive_date")
-                          val archiveDate: Option[DateTime],
-                          @Column("archive_userid")
-                          val archiveUserId: Option[Long],
-                          @Column("last_update_date")
-                          val lastUpdateDate: DateTime,
-                          val id: Option[Long] = None
-                          ) extends KeyedEntity[Option[Long]] {
+case class SupportTicketP(
+                           supportTicket: SupportTicket,
+                           applicant:Option[User],
+                           archivePerson:Option[User],
+                           lTrackings: List[Tracking] = List(),
+                           lSupportDepartments: List[Department] = List(),
+                           lSupportLeaders: List[User] = List()
+                           )
+
+/**
+ * 技术支持单表对象
+ * @param applicantId
+ * @param supportContent
+ * @param stStatus
+ * @param region
+ * @param serialNumber
+ * @param devScheDate
+ * @param psgScheDate
+ * @param devDsScheDate
+ * @param devDdScheDate
+ * @param devDtScheDate
+ * @param psgDsScheDate
+ * @param psgIsScheDate
+ * @param applyingFeedbackDate
+ * @param psgCompDate
+ * @param devCompDate
+ * @param psgDsCompDate
+ * @param psgIsCompDate
+ * @param devDsCompDate
+ * @param devDdCompDate
+ * @param devDtCompDate
+ * @param feedbackConfirmDate
+ * @param comments
+ * @param archiveCode
+ * @param archiveDate
+ * @param archiveUserId
+ * @param lastUpdateDate
+ * @param id
+ */
+class SupportTicket(
+                     @Column("applicant")
+                     val applicantId: Long,
+                     @Column("support_content")
+                     val supportContent: String,
+                     @Column("st_status")
+                     val stStatus: String,
+                     val region: String,
+                     @Column("serial_number")
+                     val serialNumber: Int,
+                     @Column("dev_sche_date")
+                     val devScheDate: Option[DateTime],
+                     @Column("psg_sche_date")
+                     val psgScheDate: Option[DateTime],
+                     @Column("dev_ds_sche_date")
+                     val devDsScheDate: Option[DateTime],
+                     @Column("dev_dd_sche_date")
+                     val devDdScheDate: Option[DateTime],
+                     @Column("dev_dt_sche_date")
+                     val devDtScheDate: Option[DateTime],
+                     @Column("psg_ds_sche_date")
+                     val psgDsScheDate: Option[DateTime],
+                     @Column("psg_Is_sche_date")
+                     val psgIsScheDate: Option[DateTime],
+                     @Column("applying_feedback_date")
+                     val applyingFeedbackDate: Option[DateTime],
+                     @Column("psg_comp_date")
+                     val psgCompDate: Option[DateTime],
+                     @Column("dev_comp_date")
+                     val devCompDate: Option[DateTime],
+                     @Column("psg_ds_comp_date")
+                     val psgDsCompDate: Option[DateTime],
+                     @Column("psg_is_comp_date")
+                     val psgIsCompDate: Option[DateTime],
+                     @Column("dev_ds_comp_date")
+                     val devDsCompDate: Option[DateTime],
+                     @Column("dev_dd_comp_date")
+                     val devDdCompDate: Option[DateTime],
+                     @Column("dev_dt_comp_date")
+                     val devDtCompDate: Option[DateTime],
+                     @Column("feedback_confirm_date")
+                     val feedbackConfirmDate: Option[DateTime],
+                     val comments: Option[String],
+                     @Column("archive_code")
+                     val archiveCode: Option[String],
+                     @Column("archive_date")
+                     val archiveDate: Option[DateTime],
+                     @Column("archive_userid")
+                     val archiveUserId: Option[Long],
+                     @Column("last_update_date")
+                     val lastUpdateDate: DateTime,
+                     val id: Option[Long] = None
+                     ) extends KeyedEntity[Option[Long]] {
+
+  def canEqual(other: Any): Boolean = other.isInstanceOf[SupportTicket]
+
+  override def equals(other: Any): Boolean = other match {
+    case that: SupportTicket =>
+      super.equals(that) &&
+        (that canEqual this) &&
+        applicantId == that.applicantId &&
+        supportContent == that.supportContent &&
+        stStatus == that.stStatus &&
+        region == that.region &&
+        serialNumber == that.serialNumber &&
+        devScheDate == that.devScheDate &&
+        psgScheDate == that.psgScheDate &&
+        devDsScheDate == that.devDsScheDate &&
+        devDdScheDate == that.devDdScheDate &&
+        devDtScheDate == that.devDtScheDate &&
+        psgDsScheDate == that.psgDsScheDate &&
+        psgIsScheDate == that.psgIsScheDate &&
+        applyingFeedbackDate == that.applyingFeedbackDate &&
+        psgCompDate == that.psgCompDate &&
+        devCompDate == that.devCompDate &&
+        psgDsCompDate == that.psgDsCompDate &&
+        psgIsCompDate == that.psgIsCompDate &&
+        devDsCompDate == that.devDsCompDate &&
+        devDdCompDate == that.devDdCompDate &&
+        devDtCompDate == that.devDtCompDate &&
+        feedbackConfirmDate == that.feedbackConfirmDate &&
+        comments == that.comments &&
+        archiveCode == that.archiveCode &&
+        archiveDate == that.archiveDate &&
+        archiveUserId == that.archiveUserId &&
+        lastUpdateDate == that.lastUpdateDate &&
+        id == that.id
+    case _ => false
+  }
+
+  override def hashCode(): Int = {
+    val state = Seq(super.hashCode(), applicantId, supportContent, stStatus, region, serialNumber, devScheDate, psgScheDate, devDsScheDate, devDdScheDate, devDtScheDate, psgDsScheDate, psgIsScheDate, applyingFeedbackDate, psgCompDate, devCompDate, psgDsCompDate, psgIsCompDate, devDsCompDate, devDdCompDate, devDtCompDate, feedbackConfirmDate, comments, archiveCode, archiveDate, archiveUserId, lastUpdateDate, id)
+    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+  }
 }
 
 /**
@@ -256,7 +336,7 @@ case class Attachment(
                        ) extends KeyedEntity[Option[Long]]
 
 case object Techsupport extends Schema {
-  val supportTickets = table[SupportTicketTable]("t_ts_tech_support")
+  val supportTickets = table[SupportTicket]("t_ts_tech_support")
   on(supportTickets)(s => declare(s.id is(autoIncremented("SEQ_TS_ST"), primaryKey)))
   val trackings = table[Tracking]("t_ts_tracking")
   on(trackings)(t => declare(t.id is(autoIncremented("seq_ts_track"), primaryKey)))
