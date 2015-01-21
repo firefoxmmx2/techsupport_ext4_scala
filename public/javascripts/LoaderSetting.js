a861d217e2a18d3d5f585359b3cdf9012168c046
firefoxmmx2 <firefoxmmx@gmail.com>
2015-01-21 23:44:21 +0800

    update LoaderSetting.js => Ext.ux

--- public/javascripts/LoaderSetting.js
+++ public/javascripts/LoaderSetting.js
@@ -3,5 +3,5 @@
  */
 //Ext.Loader.setPath('Ext' , 'assets/javascripts/ext')
 //Ext.Loader.setPath('Techsupport','assets/javascripts/application/systemmanage/app')
-Ext.Loader.setPath('Ext.uxx','assets/javascripts/app/component')
+Ext.Loader.setPath('Ext.ux','assets/javascripts/app/component')
 //alert(Ext.Loader.getPath("T`echsupport"))
