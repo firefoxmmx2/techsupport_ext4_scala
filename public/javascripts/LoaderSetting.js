dc0dac64727b10f1765b66e4e55b4d78433cd664
firefoxmmx2 <firefoxmmx@gmail.com>
2015-01-21 23:37:05 +0800

    add LoaderSetting

---
+++ public/javascripts/LoaderSetting.js
@@ -0,0 +1,7 @@
+/**
+ * Created by hooxin on 15-1-21.
+ */
+//Ext.Loader.setPath('Ext' , 'assets/javascripts/ext')
+//Ext.Loader.setPath('Techsupport','assets/javascripts/application/systemmanage/app')
+Ext.Loader.setPath('Ext.uxx','assets/javascripts/app/component')
+//alert(Ext.Loader.getPath("T`echsupport"))
